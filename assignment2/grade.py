750fd44fbfee5144fd0d2446a8a05c9991a58eae
florian490 <florian@faedler.com>
2026-06-07 13:33:51 +0200

    matr hinzugefügt

--- assignment2/grade.py
+++ assignment2/grade.py
@@ -4,7 +4,7 @@
 # Author 2:      Flo 1
 # MatNr 2:       12403729
 # Author 3:      Flo 2
-# MatNr 3:       01234567
+# MatNr 3:       12422306
 # File:          grade.py
 # Description:   Contains the Grade class.
 # Comments:      nothing to add.
