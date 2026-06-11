fcb708d216d1cbda5e73ee977817689c549fd395
Dein Name <deine.email@example.com>
2026-06-11 19:44:23 +0200

    removal of comments

--- assignment2/enrollment.py
+++ assignment2/enrollment.py
@@ -37,20 +37,14 @@ class Enrollment:
         return f"Total number of enrollments: {cls.total_enrollments}"
 
     def __lshift__(self, grade: Grade) -> Enrollment:
-        """Fügt eine Note über den << Operator hinzu."""
         self.grades.append(grade)
 
         return self
 
     def __len__(self) -> int:
-        """Gibt die Anzahl der gespeicherten Noten zurück."""
         return len(self.grades)
 
     def __call__(self, threshold: float) -> list[Grade]:
-        """
-        Macht das Objekt aufrufbar. Gibt alle Noten zurück,
-        die größer oder gleich dem threshold sind.
-        """
         return [g for g in self.grades if g.value >= threshold]
 
     def __repr__(self) -> str:
